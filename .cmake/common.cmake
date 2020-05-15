3caae29aa0782f274806a3ada474a4dda80d259f
toebsen <toebsen@hotmail.com>
2020-05-15 09:17:33 +0200

    add travis ci with g++-9

--- .cmake/common.cmake
+++ .cmake/common.cmake
@@ -1,12 +1,25 @@
 set(CMAKE_CXX_STANDARD 20)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_BUILD_TYPE Debug)
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
 
 add_definitions(-DDEBUG_)
 
+message(STATUS "ENABLE_COVERAGE=${ENABLE_COVERAGE}")
+
+
+set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
+
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     # using Clang
-    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=address -g")
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    # using GCC
+    if(ENABLE_COVERAGE)
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g ")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs")
+        set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftest-coverage")
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lgcov --coverage")
+    endif()
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
     # using Intel C++
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
