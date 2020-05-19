53ac8ba5b4ee33c740f0309704c893337d7a9859
toebsen <toebsen@hotmail.com>
2020-05-19 12:55:27 +0200

    add coverage with cpp-coveralls

--- .cmake/common.cmake
+++ .cmake/common.cmake
@@ -5,21 +5,20 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++20")
 
 add_definitions(-DDEBUG_)
 
-message(STATUS "ENABLE_COVERAGE=${ENABLE_COVERAGE}")
-
-
 set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
 
 if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     # using Clang
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "GNU")
-    if(ENABLE_COVERAGE)
+    if (ENABLE_COVERAGE)
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -g ")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -O0")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fprofile-arcs")
         set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -ftest-coverage")
         set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -lgcov --coverage")
-    endif()
+    else ()
+        message(STATUS "ENABLE_COVERAGE=OFF")
+    endif ()
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "Intel")
     # using Intel C++
 elseif (CMAKE_CXX_COMPILER_ID STREQUAL "MSVC")
